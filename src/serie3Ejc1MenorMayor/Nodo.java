9b4f34a0a3aa93d915a38c7fc1a09fdd560eb302
Olga Lucia <Olga Lucia@DESKTOP-D5J3FSP>
2019-08-18 15:09:12 -0500

    Serie3_Ejc

---
+++ src/serie3Ejc1MenorMayor/Nodo.java
@@ -0,0 +1,16 @@
+
+package serie3Ejc1MenorMayor;
+
+
+public class Nodo {
+    
+    
+    int numero;
+    Nodo siguiente;
+    
+    public Nodo(int numero){
+        this.numero = numero;
+        siguiente = null;
+}
+    
+}
